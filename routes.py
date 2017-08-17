789ca7a59c124d9fe5f2e0de1de8b34eff6884bc
z3463382 <mldelaney94@gmail.com>
2017-08-17 20:53:02 +1000

    Added a modular Hello World server for localhost

--- routes.py
+++ routes.py
@@ -1,12 +1,6 @@
-#
+#!/usr/bin/python3
 from flask import Flask
-# create a Flask application instance
-app = Flask(__name__)
-# define a route through the app.route decorator
+from server import app
 @app.route("/")
 def index():
-        return "<h1>Hello World!</h1>"
-# launch the integrated development web server
-# and run the app on http://localhost:8085
-        if __name__=="__main__":
-               app.run(debug=True,port=8085)
+    return "<h1>Oh</h1>"
---
+++ run.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from routes import app
+app.run(debug=True, port=8085)
